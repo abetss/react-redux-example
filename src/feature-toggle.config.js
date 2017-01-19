4669e88c144812d44f9cfacdba802065acd4d008
Abtin Ghods <abtin.ghods@gmail.com>
2017-01-19 17:30:14 -0500

    Add clean up instructions

--- src/feature-toggle.config.js
+++ src/feature-toggle.config.js
@@ -1,7 +1,11 @@
 export const featureToggleConfig = {
   'counter-button': {
     variant: 'button-aqua',
-    description: '',
-    cleanUpHowTo: '',
+    description: 'the button-aque is default button with aqua color',
+    cleanUpHowTo: `
+        in the src/components/button 
+        - copy paste the code of button-default.js or button-aqua to index.js
+        - delete both button-default.js or button-aqua
+    `,
   },
 };
