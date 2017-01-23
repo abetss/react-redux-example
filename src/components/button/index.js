0676d698db8f8c814d5ee98a00185cc73a98a8c8
Abtin Ghods <abtin.ghods@gmail.com>
2017-01-23 11:24:51 -0500

    Implement a simple toggle with optimizely

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,10 +2,33 @@ import React from 'react';
 import ButtonDefault from './button-default';
 import ButtonAqua from './button-aqua';
 import { FeatureToggle, Variant, Default } from '../../containers/feature-toggle';
+// import optimizely from 'optimizely-client-sdk';
+// import axios from 'axios';
+
+// let optimizelyClientInstance;
+// const url = 'https://cdn.optimizely.com/json/8169608286.json';
+// axios.get(url)
+//   .then(datafile => {
+//     window.console.log(datafile);
+//     optimizelyClientInstance = optimizely.createInstance({ datafile: datafile.json() });
+//   })
+//   .catch(error => {
+//     window.console.log(error);
+//   });
 
 function Button({
   ...props
 }) {
+  // window.console.log('optimzely client', optimizelyClientInstance);
+  // const variationKey = optimizelyClientInstance.activate('counter-button', userId);
+  // if (variationKey === 'default') {
+  //   window.console.log('variation', variationKey);
+  // } else if (variationKey === 'button-aqua') {
+  //   window.console.log('variation', variationKey);
+  // } else {
+  //   window.console.log('no variation', variationKey);
+  // }
+
   return (
     <FeatureToggle name="counter-button">
       <Default>
